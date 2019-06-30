231b6a1d8883b69752a274013d8b70d36e98c3e0
ghetto <ghetto.ch@gmail.com>
2019-06-30 10:12:46 +0200

    first commit

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "mono:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -84,25 +84,53 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
+
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	/* "black", */
+	/* "red3", */
+	/* "green3", */
+	/* "yellow3", */
+	/* "blue2", */
+	/* "magenta3", */
+	/* "cyan3", */
+	/* "gray90", */
+
+	"#181818", /* base00 */
+	"#ab4642", /* base08 */
+	"#a1b56c", /* base0B */
+	"#f7ca88", /* base0A */
+	"#7cafc2", /* base0D */
+	"#ba8baf", /* base0E */
+	"#86c1b9", /* base0C */
+	"#d8d8d8", /* base05 */
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	/* "gray50", */
+	/* "red", */
+	/* "green", */
+	/* "yellow", */
+	/* "#5c5cff", */
+	/* "magenta", */
+	/* "cyan", */
+	/* "white", */
+
+	"#585858", /* base03 */
+	"#ab4642", /* base08 */
+	"#a1b56c", /* base0B */
+	"#f7ca88", /* base0A */
+	"#7cafc2", /* base0D */
+	"#ba8baf", /* base0E */
+	"#86c1b9", /* base0C */
+	"#f8f8f8", /* base07 */
+
+	/* 256 colors */
+
+	"#dc9656", /* base09 */
+	"#a16946", /* base0F */
+	"#282828", /* base01 */
+	"#383838", /* base02 */
+	"#b8b8b8", /* base04 */
+	"#e8e8e8", /* base06 */
 
 	[255] = 0,
 
@@ -117,9 +145,9 @@ static const char *colorname[] = {
  * foreground, background, cursor, reverse cursor
  */
 unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultbg = 16;
+static unsigned int defaultcs = 13;
+static unsigned int defaultrcs = 0;
 
 /*
  * Default shape of cursor
